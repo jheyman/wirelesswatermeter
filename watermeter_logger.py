0d3a990ee050857e0618b862d29d5a7aff62335a
Julien Heyman <bidsomail at gmail.com>
2015-01-02 22:15:25 +0100

    minor fix

--- watermeter_logger.py
+++ watermeter_logger.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Credits to python port of nrf24l01, Joao Paulo Barrac & maniacbugs original c library, and this blog: http://blog.riyas.org
 
 from nrf24 import NRF24
 import time
 
-pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+pipes = [[0xf0, 0xf0, 0xf0, 0xf0, 0xe1], [0xf0, 0xf0, 0xf0, 0xf0, 0xd2]]
 
 radio = NRF24()
-radio.begin(0, 0, 25, 7)
-
+radio.begin(0, 0,25,7) #set gpio 25 as CE pin
 radio.setRetries(15,15)
-
 radio.setPayloadSize(32)
-radio.setChannel(0x60)
+radio.setChannel(0x4c)
 radio.setDataRate(NRF24.BR_250KBPS)
-radio.setPALevel(NRF24.PA_MIN)
-
-radio.setAutoAck(True)
-radio.enableDynamicPayloads()
-radio.enableAckPayload()
-
+radio.setPALevel(NRF24.PA_MAX)
+radio.setAutoAck(1)
 radio.openWritingPipe(pipes[0])
 radio.openReadingPipe(1, pipes[1])
 
@@ -27,17 +22,13 @@ radio.startListening()
 radio.stopListening()
 
 radio.printDetails()
-
 radio.startListening()
 
-c=1
 while True:
     pipe = [0]
-    # wait for incoming packet from transmitter
-    while not radio.available(pipe):
-        time.sleep(10000/1000000.0)
-
+    while not radio.available(pipe, True):
+        time.sleep(1000/1000000.0)
     recv_buffer = []
-    radio.read(recv_buffer, radio.getDynamicPayloadSize())
-    print recv_buffer
-
+    radio.read(recv_buffer)
+    out = ''.join(chr(i) for i in recv_buffer)
+    print out
